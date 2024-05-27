64d0d4fb06daa9da26bc08cf85ebd52c70eaa78f
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-27 16:26:29 +0200

    add changes functions

--- app/Models/project.php
+++ app/Models/project.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project as XmlProject;
 
 class project extends Model
 {
     use HasFactory;
 
-    // protected $fillable= ['title', 'slug'];
+    //  protected $fillable= ['title', 'slug'];
+    public function project(){
+        return $this->belongsTo(Project::class)
+
+    }
 }
